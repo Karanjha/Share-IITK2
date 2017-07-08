e271d6b95837108abb1abacae9a459a637390e93
Karan Jha <karan.jha56@yahoo.com>
2017-07-08 11:12:43 +0530

    updated dashboard component

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import {File} from './file';
- const FILES: File[]=[
-    {name: 'Intro to programming'},
-    {name: 'Quantum Chemistry'},
-    {name: 'ODE'},
-    {name: 'linear algebra'},
-    {name: 'intro to electrodynamics'}
- ]
+import {FILES} from './files';
+
  
  @Component({
     selector: 'dashboard',
-    template: `
-    <table class="files" cellspacing="500">
-    
-    <tr *ngFor="let file of files">
-     <td>{{file.name}}</td>  
-     
-      <td> <a>Download</a> <td>  
-      </tr>
-    
-    
-    </table>
-    ` ,
+    templateUrl: './database.component.html',
     styleUrls: ['./app.component.css']
  })
 
